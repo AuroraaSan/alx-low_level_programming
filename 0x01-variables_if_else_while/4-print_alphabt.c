1ae6d19425c9606f01c01bfd9d96b5bfc0bd3a55
root <root@AuroraSan>
2023-06-16 20:51:58 +0300

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing lowercase alphabets except q and e
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		if (n != 'e' && n != 'q')
+		{
+		putchar(n);
+		}
+	}
+	return (0);
+}
