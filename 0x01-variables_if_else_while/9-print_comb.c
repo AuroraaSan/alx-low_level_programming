5e233290a570064988760550e6f45572bed334cf
root <root@AuroraSan>
2023-06-16 23:25:57 +0300

    gg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Description: prints combinations
  *
- * Return: 0 (Success)
+ * Return: 0 (Scuccess)
 */
 int main(void)
 {
-    int n;
-    int sim = 44;
-    int sp = 32;
-
-    for (n = 48; n <= 57; n++)
-    {
-        putchar(n);
-        putchar(sim);
-
-        if (n != 57) // Add condition to avoid printing comma after the last number
+        int n;
+        int sim = 44;
+        int sp = 32;
+        for(n = 48 ; n <= 57 ; n++)
         {
-            putchar(sp);
+                if( n == 57)
+                {
+                        sim = 32;
+                }
+                putchar(n);
+                putchar(sim);
+                putchar(sp);
         }
-    }
-
-    putchar('\n');
-
-    return 0;
+        putchar('\n');
+        return(0);
 }
-
