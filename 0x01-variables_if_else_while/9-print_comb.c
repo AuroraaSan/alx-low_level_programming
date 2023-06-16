97fada9aa487b3205a9af406a077598971093a0b
root <root@AuroraSan>
2023-06-16 23:49:31 +0300

    gg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,19 @@
 */
 int main(void)
 {
-int n;
-int sim = 44;
-int sp = 32;
-for(n = 48;n<= 57;n++)
-{
-        if (n != 48) // Add condition to avoid printing space before the first number
+        int n;
+        int sim = 44;
+        int sp = 32;
+        for(n = 48 ; n <= 57 ; n++)
         {
-            putchar(sp);
+                if( n == 57)
+                {
+                        sim = 32;
+                }
+                putchar(n);
+                putchar(sim);
+                putchar(sp);
         }
-
-        putchar(n);
-        putchar(sim);
-    }
-
-    return 0;
+        putchar('\n');
+        return(0);
 }
-
