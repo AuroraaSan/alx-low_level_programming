aa21c09c2afffb81a02f4c40e027bdeff97aae8c
root <root@AuroraSan>
2023-06-16 22:47:56 +0300

    kk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,6 @@ int main(void)
 {
 	int n;
 	int sim = 44;
-	int doll = 36;
 	int sp = 32;
 
 	for (n = 48 ; n <= 57 ; n++)
@@ -20,9 +19,11 @@ int main(void)
 		if (n == 57)
 		{
 			sim = 36;
+			sp = 0;
 		}
 		putchar(n);
 		putchar(sim);
+		putchar(sp);
 	}
 	putchar('\n');
 	return (0);
