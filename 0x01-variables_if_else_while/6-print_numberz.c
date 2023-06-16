3465229961dffec8c38fb7f86c0f3969c23f58b4
root <root@AuroraSan>
2023-06-16 22:25:40 +0300

    ll

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	} putchar('\n');
+	return (0);
+}
