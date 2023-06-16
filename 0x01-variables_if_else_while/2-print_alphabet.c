52bbae4923ca7436dbe5409d995f7fa2872a4763
root <root@AuroraSan>
2023-06-16 20:17:06 +0300

    alll

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints lowercase alphabet
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 'a';
+
+	for (n ; n <= 122; n++)
+		putchar(n);
+	return (0);
+}
