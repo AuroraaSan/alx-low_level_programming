5ce91924aeb9ea3cc63d92f7006c205f694a2f22
root <root@AuroraSan>
2023-06-17 05:14:53 +0300

    kk

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: three combinations
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0, k, j;
+
+        while (n <= 9)
+        {
+                k = 0;
+                while (k <= 9)
+                {
+			j = 0;
+			while (j <= 9)
+			{
+                        	if (n != k && n < k && k != j && k < j)
+                        	{
+                                	putchar(n + 48);
+                                	putchar(k + 48);
+					putchar(j + 48);
+                                	if (n + k + j != 24)
+                                	{	
+                                        	putchar(',');
+                                        	putchar(' ');
+                                	}
+                        	}
+				j++;
+			}
+			k++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
