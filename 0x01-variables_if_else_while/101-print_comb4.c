9ff4237b9e87270ab871a1b32d2bac1b9a404320
root <root@AuroraSan>
2023-06-17 05:18:04 +0300

    jj

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,25 +12,25 @@ int main(void)
 {
 	int n = 0, k, j;
 
-        while (n <= 9)
-        {
-                k = 0;
-                while (k <= 9)
-                {
+	while (n <= 9)
+	{
+		k = 0;
+		while (k <= 9)
+		{
 			j = 0;
 			while (j <= 9)
 			{
-                        	if (n != k && n < k && k != j && k < j)
-                        	{
-                                	putchar(n + 48);
-                                	putchar(k + 48);
+				if (n != k && n < k && k != j && k < j)
+				{
+					putchar(n + 48);
+					putchar(k + 48);
 					putchar(j + 48);
-                                	if (n + k + j != 24)
-                                	{	
-                                        	putchar(',');
-                                        	putchar(' ');
-                                	}
-                        	}
+					if (n + k + j != 24)
+					{	
+						putchar(',');
+						putchar(' ');
+					}
+				}
 				j++;
 			}
 			k++;
