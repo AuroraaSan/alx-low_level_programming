94924e12bd7c428eaa53fba075489c6f558c69b2
root <root@AuroraSan>
2023-06-17 05:03:59 +0300

    jj

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: possible combinations of two numbers
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0, k;
+
+	while (n <= 9)
+	{
+		k = 0;
+		while ( k <= 9)
+		{
+			if (n != k && n < k)
+			{
+				putchar(n+48);
+				putchar(k+48);
+				
+				if (n + k != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			k++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
