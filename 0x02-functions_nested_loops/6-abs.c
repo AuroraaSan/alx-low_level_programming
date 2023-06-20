1fad4b1eab489983e962ea2908984faead43d41d
root <root@AuroraSan>
2023-06-20 21:01:41 +0300

    kk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - gets absolute of number
+ * @int: input of function
+*/
+
+int _abs(int)
+{
+	if (int == 0)
+	{
+		_putchar('0');
+	} else if (int < 0)
+	{
+		_putchar(abs(int));
+	}
+	_putchar(int);
+	_putchar('\n');
+}
