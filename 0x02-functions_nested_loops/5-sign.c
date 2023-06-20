e80e3313e3324f66534a266b3d73c693337e6260
root <root@AuroraSan>
2023-06-20 20:56:41 +0300

    kk

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,7 @@
 
 int print_sign(int n)
 {
-	if(n == 0)
+	if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
@@ -17,7 +17,7 @@ int print_sign(int n)
 	{
 		_putchar ('-');
 		return (-1);
-	} 
+	}
 	_putchar ('+');
-	return(1);
+	return (1);
 }
