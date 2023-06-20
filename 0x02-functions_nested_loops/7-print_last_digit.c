acf7e104d2a2d8b2dc69d9e14b01fd90f32cab8c
root <root@AuroraSan>
2023-06-20 21:09:57 +0300

    kk

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: input function
+ *
+ * Return: value of last digit
+*/
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
