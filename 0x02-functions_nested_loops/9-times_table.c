de425e7d1b67c756b9d2282c983152cab12b1242
root <root@AuroraSan>
2023-06-20 21:47:09 +0300

    ll

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,27 +8,22 @@ void times_table(void)
 {
 	int num;
 	int line;
-	int n;
-	int prod;
 
-	for (line = 1; line <= 10; line ++)
+	for (line = 0; line <= 9; line++)
 	{
 		_putchar(48);
 		for (num = 1; num <= 9 ; num++)
 		{
-			for (n = 1; n <= 9; n++)
+			_putchar(',');
+			_putchar(' ');
+			prod = line * num;
+			if (prod <= 9)
 			{
-				_putchar(',');
 				_putchar(' ');
-				prod = n * num;
-				if (prod <= 9)
-				{
-					_putchar(' ');
-				} else
-					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
-			}
+			} else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
