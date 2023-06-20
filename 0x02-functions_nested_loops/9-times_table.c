7ee27651509a1108683abf0214b1167ff36bc11f
root <root@AuroraSan>
2023-06-20 21:44:11 +0300

    ll

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,41 +8,26 @@ void times_table(void)
 {
 	int num;
 	int line;
+	int n;
+	int prod;
 
 	for (line = 1; line <= 10; line ++)
 	{
+		_putchar(48);
 		for (num = 1; num <= 9 ; num++)
 		{
-			_putchar((num * 0) + 48);
-			_putchar(',');
-			_putchar(' ');
-			_putchar((num * 1) + 48);
-			_putchar(',');
-                        _putchar(' ');
-			_putchar((num * 2) + 48);
-                        _putchar(',');
-                        _putchar(' ');
-			_putchar((num * 3) + 48);
-                        _putchar(',');
-                        _putchar(' ');
-			_putchar((num * 4) + 48);
-                        _putchar(',');
-                        _putchar(' ');
-			_putchar((num * 5) + 48);
-                        _putchar(',');
-                        _putchar(' ');
-			_putchar((num * 6) + 48);
-                        _putchar(',');
-                        _putchar(' ');
-			_putchar((num * 7) + 48);
-                        _putchar(',');
-                        _putchar(' ');
-			_putchar((num * 8) + 48);
-                        _putchar(',');
-                        _putchar(' ');
-			_putchar((num * 9) + 48);
-                        _putchar(',');
-                        _putchar(' ');
+			for (n = 1; n <= 9; n++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = n * num;
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				} else
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}
