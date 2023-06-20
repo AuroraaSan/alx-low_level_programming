0554434704cacec5f300bcc829e319a92c98b581
root <root@AuroraSan>
2023-06-20 21:51:12 +0300

    ll

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add to integers
+ * @x: first integer
+ * @y: second one
+ *
+ * Return: the value of sum
+*/
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
