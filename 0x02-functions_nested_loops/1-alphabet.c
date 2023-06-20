87af96f26afb8ef73d75d78e65e0be4535c5b439
root <root@AuroraSan>
2023-06-20 19:54:03 +0300

    uu

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * prints lowercase alphabet
+*/
+void print_alphabet(void)
+{
+	int car;
+
+	for (car = 'a'; car <= 'z'; car++)
+	{
+		_putchar(car);
+	}
+	_putchar('\n');
+}
