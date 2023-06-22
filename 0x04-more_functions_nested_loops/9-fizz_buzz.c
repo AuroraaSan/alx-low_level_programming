2eedafa359d0121b6e7412b45002ebcf4ae05be8
root <root@AuroraSan>
2023-06-22 22:35:11 +0300

    8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * fizzbuzz - prints numbers from 1 to 100*/
+
+void fizzbuzz(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ( x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz ");
+		else if (x % 3 == 0)
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", x);
+	} printf('\n');
+}
