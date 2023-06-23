f7adbb706f2c3b055db73aea4c22e4786cb004cf
root <root@AuroraSan>
2023-06-23 04:11:11 +0300

    kk

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+*/
+
+void print_triangle(int size)
+{
+	int row, num, x;
+	
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (num = 1; num <= size; num++)
+			{
+				if ((row + num) <= size)
+					_putchar(' ');
+				else
+					_putchar('0');
+			}
+			_putchar('\n');
+		}
+	}
+}
