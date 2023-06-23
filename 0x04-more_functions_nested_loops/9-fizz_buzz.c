2735f9cdbfc40d36afd8e8333af2e508fdcd0626
root <root@AuroraSan>
2023-06-23 03:29:58 +0300

    dfd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,12 @@ void main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
-			printf("FizzBuzz");
-		else if (x % 3 == 0)
+		if (x % 3 == 0 && !(x % 5 == 0))
 			printf("Fizz");
-		else if (x % 5 == 0)
+		else if (x % 5 == 0 && !(x % 3 == 0))
 			printf("Buzz");
+		else if (x % 5 == 0 && x % 3 == 0)
+			printf("FizzBuzz");
 		else
 			printf("%d", x);
 		if (x != 100)
@@ -25,6 +25,5 @@ void main(void)
 		else
 			printf('\n');
 	}
-	printf("Buzz");
-	printf("\n");
+	return (0);
 }
