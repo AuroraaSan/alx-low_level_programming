f3a7ff4d41bb2671dbeaed3ca7a044bdc7894007
root <root@AuroraSan>
2023-06-28 19:51:34 +0300

    oopp

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *_strcpy -  copies the string pointed to by src, including the terminating null byte (\0)
+ * *_strcpy -  copies the string pointed to
  * @dest: copied to
  * @src: the source
  *
