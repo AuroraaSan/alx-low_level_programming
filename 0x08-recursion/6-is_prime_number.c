2cc31be93596bea545f3981b92c86bd286850a88
root <root@AuroraSan>
2023-07-06 02:43:53 +0300

    rrrs

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
  * check - check prime number
  * @n: number
  * @i: integer
  * Return: result
 */
+
 int check(int n, int i)
 {
 	if (n == i && n > 1)
 		return (1);
 	if (n % i == 0 || n <= 1)
 		return (0);
-	return(check (n, i + 1));
+	return (check (n, i + 1));
 }
-
 
 /**
  * is_prime_number - returns 1 if the input integer is a prime number
