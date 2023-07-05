a8f423784affa7a91e6bea59729c2ab6026e2f63
root <root@AuroraSan>
2023-07-06 01:58:11 +0300

    ss

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,8 +11,7 @@ int _strlen_recursion(char *s)
 
 	if (*s > '\0')
 	{
-		count++;
-		_strlen_recursion(s + 1);
+		count += _strlen_recursion(s + 1) + 1;
 	}
-	return (count + 1);
+	return (count);
 }
