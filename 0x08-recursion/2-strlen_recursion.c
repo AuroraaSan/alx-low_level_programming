a8d26f65d022ff3c350ab3186660d0dd0c699ad0
root <root@AuroraSan>
2023-07-06 01:45:26 +0300

    ss

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -  returns the length of a string
+ * s: pointer to the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int count;
+
+	if (*s > '\0')
+	{
+		count++;
+		_strlen_recursion(*s + 1);
+	}
+}
