b4f644828edb6b091a843f22a81dd59603929605
root <root@AuroraSan>
2023-07-11 19:38:45 +0300

    Ss

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 		char* arr;
-		int i;
+		unsigned int i;
 
 		arr = (char*)malloc(size * sizeof(char));
 		for (i = 0; i < size * sizeof(char); i++)
