3ffd3bcb04c4fad4c313ce585b15ac315d41428a
root <root@AuroraSan>
2023-07-13 20:07:40 +0300

    g

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (lenn1 = 0; s1[lenn1] != '\0'; lenn1++)
 		;
 	for (lenn2 = 0; s2[lenn2] != '\0'; lenn2++)
-			;
+		;
 	str = malloc(lenn1 + n + 1);
 	if (str == NULL)
 		return (NULL);
