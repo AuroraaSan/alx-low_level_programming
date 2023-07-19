73d6ea170260f7cbc6706704f70dc97539705117
root <root@AuroraSan>
2023-07-20 00:45:10 +0300

    Dd

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: name
+ * @age: age
+ * @owner: name
+ *
+ * Description: dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
