6914ed14289eab94a5569478338459efefdb7dd4
root <root@AuroraSan>
2023-07-20 23:21:17 +0300

    ss

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -13,12 +13,13 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 		printf("Error\n"), exit(98);
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
-		printf("Error\N"), exit(99);
+		printf("Error\n"), exit(99);
 	if (!b && (argv[2][0] == '/' || agrc[2][0] == '%'))
 		printf("Error\n"), exit(100);
 	printf("%d\n", op_func(a, b));
