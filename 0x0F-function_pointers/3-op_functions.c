7b303307cd989a329cf3f3cbd7c2e076fb6126df
root <root@AuroraSan>
2023-07-20 23:06:39 +0300

    ss

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds the two integers a and b
+ * @a: the first integer
+ * @b: the ssecond integer
+ * Return: result
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract the two integers a and b
+ * @a: the first integer
+ * @b: the ssecond integer
+ * Return: result
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies the two integers a and b
+ * @a: the first integer
+ * @b: the ssecond integer
+ * Return: result
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide the two integers a and b
+ * @a: the first integer
+ * @b: the ssecond integer
+ * Return: result
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulus of the two integers a and b
+ * @a: the first integer
+ * @b: the ssecond integer
+ * Return: result
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ * @argc: number of args
+ * @argv: the vectors
+ * Return : 0
+*/
+
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int), a,b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\N"), exit(99);
+	if (!b && (argv[2][0] == '/' || agrc[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
