d2338126f99236ca90474d399e0da9ee0f31208e
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-08-01 01:21:56 +0300

    Sss

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - free list
+ * @head: pointer
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
