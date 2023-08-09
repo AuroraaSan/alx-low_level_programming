17ecb8e7fc057dc5960ee615ef22e6633a2dc3b4
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-08-10 00:41:45 +0300

    Ddd

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @index: index
  * Return: value
 */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
