9bb28cd91209c776be3b848960dabcbaf259c952
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-08-10 00:19:00 +0300

    Sss

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * bianry_to_uint - converts binary to unsigned int
+ * @b: number in binary
+ * Return: result
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0)
+				num = num * 2 + (*b++ - '0');
+	}
+	return (num);
+}
