6c7b12de35057e4d279db9f4c85e114b530585f9
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-08-10 03:16:09 +0300

    dd

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <corecrt.h>
 #include <sys/stat.h>
 
 #define USAGE "Usage: cp file_from file_to\n"
@@ -21,31 +20,31 @@ int main(int ac, char **av)
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
-		fprintf(STDERR_FILENO, USAGE), exit(97);
+		fprintf(stderr, USAGE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
-		fprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+		fprintf(stderr, ERR_NOREAD, av[1]), exit(98);
 
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 
 	if (to_fd == -1)
-		fprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+		fprintf(stderr, ERR_NOWRITE, av[2]), exit(99);
 
 	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 	{
 		if (write(to_fd, buf, b) != b)
-			fprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+			fprintf(stderr, ERR_NOWRITE, av[2]), exit(99);
 	}
 	if (b == -1)
 	{
-		fprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+		fprintf(stderr, ERR_NOREAD, av[1]), exit(98);
 	}
 
 	from_fd = close(from_fd);
 	to_fd = close(to_fd);
 	if (from_fd)
-		fprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+		fprintf(stderr, ERR_NOCLOSE, from_fd), exit(100);
 	if (to_fd)
-		fprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+		fprintf(stderr, ERR_NOCLOSE, from_fd), exit(100);
 	return (EXIT_SUCCESS);
 }
