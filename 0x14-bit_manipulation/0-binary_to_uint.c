1fc3ea6f413e8f1b08ec8ee84e47afbe73ae2c7a
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-09-01 19:11:01 +0300

    first

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 2;
-	unsigned int i;
-	unsigned int c;
-	unsigned int len;
+	unsigned int k;
 	
 	if (b == NULL)
 		return (0);
-	len = strlen(b);
-	for (c = len; c > 0; c--)
+	while (*b)
 	{
-		if (b[c] != '0' && b[c] != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
-		if (b[c] == '1')
-		{
-			i += atoi(b) * k;
-		}
-		k*= 2;
+		k = k * 2 + (*b++ - '0');
 	}
-	return (i);
+	return (k);
 }
