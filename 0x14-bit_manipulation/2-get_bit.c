7bfba53f019c9a83d0030bfb942370101d391761
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-09-01 22:31:38 +0300

    third

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index) 
 {
-	if (index >= (sizeof(unsigned long int) * 8)) 
+	if (index >= (sizeof(n) * 8)) 
 	{
 		return -1;
 	}
-	unsigned long int mask = 1UL << index;
-	
-	return (n & mask) ? 1 : 0;
+	return (n >> index & 1);
 }
 
