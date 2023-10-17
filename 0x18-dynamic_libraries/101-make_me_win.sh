d048a6d2f974d4344a0f17d209e236bc54cb8906
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-17 08:30:40 +0300

    ii

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/AuroraaSan/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
+wget -P https://github.com/AuroraaSan/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD= /tmp/libgiga.so
